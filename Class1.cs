83ae2a28881f4b9d4ea659da625fac661fa4bf38
TAI EMMANUEL <emmanueltai000@gmail.com>
2025-04-25 00:25:17 -0700

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,25 @@
+﻿namespace HTTPStatusLibrary
+{
+    public class Class1
+    {
+
+        public enum StatusCodesEnum
+        {
+            Ok = 200,
+            NoContent = 204,
+            Found = 302,
+            BadRequest = 400,
+            Forbidden = 403,
+            NotFound = 404,
+            BadGateway = 502,
+            GatewayTimeout = 504
+        }
+    
+    
+        public record HttpStatusDto
+        {
+            int Code { get; init; }
+            string CodeName
+        }
+    }
+}
